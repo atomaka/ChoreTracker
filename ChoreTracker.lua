704bd43c4cd11c87ff71838793734307f4801660
atomaka <atomaka@gmail.com>
2011-08-20 07:41:29 -0400

    Store Valor Points in DB for later retrieval.

---
+++ ChoreTracker.lua
@@ -0,0 +1,25 @@
+ChoreTracker = LibStub('AceAddon-3.0'):NewAddon('ChoreTracker','AceConsole-3.0','AceEvent-3.0')
+local core = ChoreTracker
+
+local defaults = {
+	profile = {
+		valorPoints = {},
+		lockouts = {},
+	}
+}
+
+function core:OnInitialize()	
+	self.db = LibStub('AceDB-3.0'):New('ChoreTrackerDB',defaults,'Default')
+end
+
+function core:OnEnable()
+	self:RegisterEvent('PLAYER_ENTERING_WORLD','StoreValorPoints')
+end
+
+function core:StoreValorPoints()
+	local _,_,_,earnedThisWeek = GetCurrencyInfo(396)
+	local name = UnitName('player')
+	
+	self.db.profile.valorPoints[name] = earnedThisWeek
+	print('Storing',earnedThisWeek,'for',name)
+end
